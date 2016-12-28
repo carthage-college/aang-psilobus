c5b6c0202096f766e44c2344b57adbb7251ae61f
skirk <skirk@suki.local>
2016-12-27 18:40:42 -0600

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { DemoService } from './demo.service';
+import { Observable } from 'rxjs/Rx';
+
+@Component({
+  selector: 'demo-app',
+  templateUrl: 'templates/app.component.tpl.html',
+})
+export class AppComponent  {
+
+  public foods;
+  public foods_error:Boolean = false;
+
+  constructor(private _demoService: DemoService) { }
+
+  ngOnInit() {
+    this.getFoods();
+  }
+
+  getFoods() {
+    this._demoService.getFoods().subscribe(
+      data => { this.foods = data},
+      err => { this.foods_error = true }
+    );
+  }
+}
---
+++ app/demo.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DemoService {
+
+    constructor(private http:Http) {
+    }
+
+    // Uses http.get() to load a single JSON file
+    getFoods() {
+        return this.http.get('/app/food.json').map((res:Response) => res.json());
+    }
+}
