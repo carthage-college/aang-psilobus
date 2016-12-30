75e4ac5099794bdcf260cb9fd6ef4b57cc0db905
skirk <skirk@suki.local>
2016-12-30 09:41:07 -0600

    new settings file ala django for global constants

--- app/demo.service.ts
+++ app/demo.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {Http, Response, Headers, RequestOptions} from "@angular/http";
 import 'rxjs/add/operator/map';
 
+import settings = require('./settings.js');
+
 @Injectable()
 export class DemoService {
 
@@ -10,6 +12,6 @@ export class DemoService {
 
     // Uses http.get() to load a single JSON file
     getFoods() {
-        return this.http.get('/mathematics/courses/syllabi/app/food.json').map((res:Response) => res.json());
+        return this.http.get(settings.root_url + 'app/food.json').map((res:Response) => res.json());
     }
 }
