f8a71655684e2ae34782e90eb78d4770d535e335
skirk <skirk@suki.local>
2017-01-01 18:31:53 -0600

    moved rest url properties to settings for now

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { DemoService } from './demo.service';
 import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'course-syllabi',
   templateUrl: 'app/app.component.tpl.html',
+  providers: [DemoService]
 })
 export class AppComponent  {
 
-  public foods;
-  public foods_error:Boolean = false;
+  public courses;
+  public courses_error:Boolean = false;
 
   constructor(private _demoService: DemoService) { }
 
   ngOnInit() {
-    this.getFoods();
+    this.getCourses();
+    //console.log(elementRef.nativeElement.getAttribute('term'));
   }
-
-  getFoods() {
-    this._demoService.getFoods().subscribe(
-      data => { this.foods = data},
-      err => { this.foods_error = true }
+  getCourses() {
+    this._demoService.getCourses().subscribe(
+      data => { this.courses = data},
+      err => { this.courses_error = true }
     );
   }
 }
--- app/demo.service.ts
+++ app/demo.service.ts
@@ -1,17 +1,16 @@
+import { Input } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from "@angular/http";
 import 'rxjs/add/operator/map';
 
-import settings = require('./settings.js');
+import settings = require('../settings.js');
 
 @Injectable()
 export class DemoService {
 
-    constructor(private http:Http) {
-    }
-
-    // Uses http.get() to communicate with REST API endpoint in json format
-    getFoods() {
-        return this.http.get(settings.root_url + 'dspace/MTH/RA/2016/courses/').map((res:Response) => res.json());
-    }
+  constructor(private http:Http) { }
+  // Uses http.get() to communicate with REST API endpoint in json format
+  getCourses() {
+    return this.http.get(settings.rest_url).map((res:Response) => res.json());
+  }
 }
